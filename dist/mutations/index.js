4146f3b88dcd6dc1caf68c9a0f6ea4c367908b0a
DiabGarren <garrendiab@gmail.com>
2023-05-16 12:31:59 +0200

    Removed dist

--- dist/mutations/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const passwordMutation = require("./password");
-const rootResolver = Object.assign({}, passwordMutation);
-module.exports = rootResolver;
--- dist/resolvers/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const passwordResolver = require("./password");
-const rootResolver = Object.assign({}, passwordResolver);
-module.exports = rootResolver;
